f9be59534b2a1b1fdd1f7a1013f235615a21354e
James Cook <mokus@deepbondi.net>
2010-07-27 19:39:15 +0000

    Initial import

---
+++ src/Math/Quadrature/Gaussian.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE 
+        StandaloneDeriving,
+        FlexibleContexts,
+        UndecidableInstances,
+        RecordWildCards
+  #-}
+module Math.Quadrature.Gaussian
+    ( Estimate(..)
+    , QRule(..)
+    , integrate, integrateRange
+    , gaussLegendre
+    ) where
+
+import Math.Quadrature.Simple (Estimate(..))
+
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic as GV
+import qualified Data.Vector.Generic.Mutable as MV
+import Math.Polynomial
+import Math.Polynomial.Legendre
+
+data QRule v a = QRule
+    { qRange    :: !(a,a)
+    , qTable    :: !(v (a,a))
+    }
+
+deriving instance (Eq   a, Eq   (v (a,a))) => Eq   (QRule v a)
+deriving instance (Show a, Show (v (a,a))) => Show (QRule v a)
+
+-- |Apply a Gaussian quadrature rule to a function.  The endpoints of integration
+-- are a property of the rule - typically, they were supplied to the function
+-- (such as 'gaussLegendre' below) that computed the parameters for the rule.
+integrate QRule{..} f = Estimate n Nothing $ sum [w * f x | (x, w) <- GV.toList qTable]
+    where n = toInteger (GV.length qTable)
+
+-- |Apply a Gaussian quadrature rule to a function, using user-specified 
+-- endpoints of integration which may differ from those for which the
+-- quadrature rule was originally defined.
+integrateRange qRule f a0 a1
+    | x0 == a0 && x1 == a1  = integrate qRule f
+    | otherwise             = integrate qRule f'
+    where
+        (x0, x1) = qRange qRule
+        scale  = (a1 - a0) / (x1 - x0)
+        
+        f' x = scale * f ((x - x0) * scale + a0)
+
+-- |Given the lower and upper limits of integration, number of points n, and
+-- desired accuracy eps, this routine returns a GaussQ quadrature rule containing
+-- the abscissas and weights of the Gauss-Legendre n-point quadrature formula.
+--
+-- Typical values for eps are 1e-15 for Double, 1e-6 for Float.  The specified
+-- eps will be approximately the accuracy of the integration method as well.
+gaussLegendre x1 x2 n eps = QRule (x1, x2) table
+    where
+        roots = GV.fromList (legendreRoots n eps)
+        derivs = GV.map (snd . evalLegendreDeriv n) roots
+        
+        table = flip GV.map (GV.zip roots derivs) $ \(z,dy) ->
+            ( {- abscissa -}    xm + xl * z
+            , {- weight   -}    2 * xl / ((1 - z*z)*dy*dy)
+            ) where
+                xm = 0.5*(x2+x1)
+                xl = 0.5*(x2-x1)
---
+++ src/Math/Quadrature/Simple.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE BangPatterns #-}
+-- |Simple quadrature schemes based on repeated subdivision, implemented
+-- as function returning series of estimates of the integral.
+-- 
+-- There is much room for stylistic and usability improvements.  Most notably,
+-- some kind of drivers should be written and integral-transforms should be
+-- implemented.  Also, it would be nice for the domain of integration to be
+-- generalized to something like range-sets.
+module Math.Quadrature.Simple where
+
+import Data.Bits
+import Data.List
+import Math.Polynomial.Interpolation
+
+data Estimate a = Estimate
+    { numPoints :: Integer
+    , stepSize  :: Maybe a  -- Maybe because not all algorithms use equally spaced points
+    , estimate  :: a
+    } deriving (Eq, Ord, Show)
+
+-- |@trapezoidRule f a b@ computes a sequence of improving estimates of the
+-- integral of f from a to b, using the trapezoid rule.
+trapezoidRule f a b = iterate next first
+    where
+        first = let h0 = (b - a)
+                 in Estimate 2 (Just h0) (0.5 * h0 * (f a + f b))
+        next (Estimate !(n+1) (Just !h0) i0) = Estimate (n+n+1) (Just h1) i1
+            where
+                h1 = 0.5 * h0
+                i1 = 0.5 * i0 + h1 * foldl1' (+) ys
+                ys = map f (midpoints h0)
+        
+        -- |Divide the interval [a,b] into segments of width dx, and return
+        -- the midpoint of each segment
+        midpoints dx = takeWhile (< max a b) (iterate (+dx) x0)
+            where
+                x0 = min a b + 0.5 * abs dx
+
+-- |@simpson'sRule f a b@ computes a sequence of improving estimates of the
+-- integral of f from a to b, using Simpson's rule.
+simpson'sRule f a b = 
+    [ Estimate n h ((4/3) * s_2n - (1/3) * s_n)
+    | Estimate _ _ s_n : Estimate n h s_2n : _ <- tails (trapezoidRule f a b)
+    ]
+
+
+midpointRule f a b = iterate next first
+    where
+        h0 = b-a
+        first = Estimate 1 (Just h0) (h0 * f (0.5 * (a+b)))
+        extension x0 hop skip = x0 : extension (x0 + skip) skip hop
+        next (Estimate n0 _ i) = Estimate n1 (Just h) (i / 3 + h * sum ys)
+            where
+                n1 = 3 * n0
+                h  = h0 / fromInteger n1
+                x0 = a + 0.5 * h
+                ys = take (2 * fromInteger n0) (map f (extension x0 h (h+h)))
+
+-- |@romberg k qrule dh f a b@ uses Romberg extrapolation to improve the 
+-- series of estimates made by a quadrature rule by fitting an order-@k@ 
+-- polynomial to (stepsize^2, X) and extrapolating to stepsize=0.  @dh@ is
+-- the ratio of stepsizes in successive estimates.
+romberg k qrule f a b = 
+    [ Estimate n h (polyInterp (take k terms) 0)
+    | estimates <- tails (qrule f a b)
+    , let terms = take k [(maybe (guessH n) (^2) mbH, x) | Estimate n mbH x <- estimates]
+          Estimate n h _ = last (take k estimates)
+    ]
+    where
+        guessH n = (b-a) / fromIntegral n
+
